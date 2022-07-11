a40fa6adeb83689c380f0b490613af514b068dea
ingridkaori <ingridkaori2@gmail.com>
2022-07-11 10:16:43 -0300

    Conectando com MongoDB

--- api/index.js
+++ api/index.js
@@ -9,10 +9,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/projeto-web-fullstack')
     .then(() => {
         console.log("++ Conexão realizada com sucesso!");
-
-        // Criação do servidor
-        app.listen(port, () => {
-            console.log("Servidor em funcionamento: http://localhost:3800");
-        });
     })
     .catch(err => console.log(err));
